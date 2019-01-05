3b67c3b55e5d9ea3d3802b09760cd5f29bb686ff
francescosblendorio <francesco.sblendorio@jobrapido.com>
2019-01-05 09:33:10 +0100

    using pre-defined constants

--- src/main/java/eu/sblendorio/exercise/Category.java
+++ src/main/java/eu/sblendorio/exercise/Category.java
@@ -2,21 +2,24 @@ package eu.sblendorio.exercise;
 
 import java.math.BigDecimal;
 
+import static java.math.BigDecimal.ZERO;
+import static java.math.BigDecimal.TEN;
+
 public enum Category {
     BOOK {
-        public BigDecimal getBasicSalesTaxRate() { return BigDecimal.valueOf(0); }
+        public BigDecimal getBasicSalesTaxRate() { return ZERO; }
     },
 
     FOOD {
-        public BigDecimal getBasicSalesTaxRate() { return BigDecimal.valueOf(0); }
+        public BigDecimal getBasicSalesTaxRate() { return ZERO; }
     },
 
     MEDICAL {
-        public BigDecimal getBasicSalesTaxRate() { return BigDecimal.valueOf(0); }
+        public BigDecimal getBasicSalesTaxRate() { return ZERO; }
     },
 
     GENERIC {
-        public BigDecimal getBasicSalesTaxRate() { return BigDecimal.valueOf(10); }
+        public BigDecimal getBasicSalesTaxRate() { return TEN; }
     }
     ;
 
